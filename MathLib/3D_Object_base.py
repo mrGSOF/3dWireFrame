78f88c1ee04ddd4639cf4fd6a2b076930894397f
Guy Soffer (GSOF) <gsoffer@yahoo.com>
2024-04-13 23:18:35 -0700

    Refactoring the code

---
+++ MathLib/3D_Object_base.py
@@ -0,0 +1,42 @@
+import MatLib
+
+class State_base():
+    def __init__(self):
+        return
+
+def scale(points, scale) -> list:
+    return MathLib.MxV(points, [scale]*3)
+#    scaled = []
+#    for point in points:
+#        scaled.append(MathLib.scale_V3(point, scale))
+#    return scaled
+
+def rotate(self, points, x=0, y=0, z=0, dcm=None, state=False) -> 3D_object_base:
+    if dcm == None:
+        dcm = MathLib.DCM_XYZ(x, y, z)
+
+    for i, point in enumerate[points]:
+        points[i] = MathLib.MxV(dcm, point)
+    return points
+
+def translate(self, points, x, y, z, state=False) -> 3D_object_base:
+    for i, point in enumerate[points]:
+        points[i] = MathLib.addV(point, (x,y,z))
+    return points
+
+def getLines(self) -> list:
+    return []
+
+class 3D_object_base() -> 3D_object_base:
+    def __init__(self, obj):
+        return
+
+    def scale(self, scale, state=False) -> 3D_object_base:
+        return self
+
+    def rotate(self, x, y, z, state=False) -> 3D_object_base:
+        return self
+
+    def translate(self, x, y, z, state=False) -> 3D_object_base:
+        return self
+
