a4147c28eeddc93c1089a4b0b315a2f23f1a0def
Guy Soffer (GSOF) <gsoffer@yahoo.com>
2024-04-17 21:21:04 -0700

    Revert back to object storing points and connection (lines) separately. This reduces the number of points to transform.

--- Lib3D/Object_WireFrame.py
+++ Lib3D/Object_WireFrame.py
@@ -1,5 +1,39 @@
+from MathLib import MathLib as ML
 from Lib3D import Object_base as O
 import json
+
+def _scale(points, scale) -> list:
+    newPoints = [None]*len(points)
+    for i,point in enumerate(points):
+        newPoints[i] = ML.scale_V3(point, scale)
+    return newPoints
+
+def _rotate(points, x=0, y=0, z=0, dcm=None, state=False) -> list:
+    newPoints = [None]*len(points)
+    if dcm == None:
+        dcm = ML.DCM_XYZ(x, y, z)
+
+    for i, point in enumerate(points):
+        newPoints[i] = ML.MxV(dcm, point)
+    return newPoints
+
+def _translate(points, x=0, y=0, z=0, V=None, state=False) -> list:
+    newPoints = [None]*len(points)
+    if V == None:
+        V = (x, y, z)
+
+    for i, point in enumerate(points):
+        newPoints[i] = ML.addV(point, V)
+    return newPoints
+
+def _calcLines(points, connections) -> list:
+    shape = [0]*len(connections)
+    for i, connect in enumerate(connections):
+        fromPnt, toPnt = connect
+        p0 = points[fromPnt]
+        p1 = points[toPnt]
+        shape[i] = (p0,p1)
+    return shape
 
 class Object_wireFrame(O.Object_base):
     def __init__(self, obj=None, filename=None, color=(0,0,0)):
@@ -7,7 +41,8 @@ class Object_wireFrame(O.Object_base):
             obj = self._loadJson(filename)
 
         self.color  = color
-        self.initShape = self._calcShape(obj["points_xyz"], obj["connections"])
+        self.initShape   = obj["points_xyz"]
+        self.connections = obj["connections"]
         self.scale(obj["scale"], initShape=True)
 
     def _updateShape(self, shape, initShape=False):
@@ -21,50 +56,23 @@ class Object_wireFrame(O.Object_base):
             obj = json.load(f)
         return obj
 
-    def _calcShape(self, points, connections) -> list:
-        shape = [0]*len(connections)
-        for i, connect in enumerate(connections):
-            fromPnt, toPnt = connect
-            p0 = points[fromPnt]
-            p1 = points[toPnt]
-            shape[i] = (p0,p1)
-        return shape
-
     def scale(self, scale, initShape=False):
-        shape = O.scale(self.initShape, scale)
+        shape = _scale(self.initShape, scale)
         self._updateShape( shape, initShape )
         return self
         
     def rotate(self, x=0, y=0, z=0, dcm=None, initShape=False):
-        shape = O.rotate( self.initShape, x,y,z, dcm )
+        shape = _rotate( self.initShape, x,y,z, dcm )
         self._updateShape( shape, initShape )
         return self
 
     def translate(self, x=0, y=0, z=0, V=None, initShape=False):
-        shape = O.translate( self.initShape, x,y,z, V )
+        shape = _translate( self.initShape, x,y,z, V )
         self._updateShape( shape, initShape )
         return self
 
     def getShape(self) -> list:
-        return self.shape
+        return self.getShape()
 
     def getLines(self) -> list:
-        return self.shape
-
-##    def setLines(self, points, connections) -> list:
-##        lines = [0]*len(connections)
-##        for i,line in enumerate(connections):
-##            fromPnt, toPnt = line
-##            p0 = self.points[fromPnt]
-##            p1 = self.points[toPnt]
-##            lines[i]=(p0,p1)
-##        return lines
-
-##    def getLines(self) -> list:
-##        lines = [0]*len(self.lines)
-##        for i,line in enumerate(self.shapes):
-##            fromPnt, toPnt = line
-##            p0 = self.newPoints[fromPnt]
-##            p1 = self.newPoints[toPnt]
-##            lines[i]=(p0,p1)
-##        return lines
+        return _calcLines(self.shape, self.connections)
