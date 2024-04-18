99742525b2996103e139cc390d75281e3daf00d4
Guy Soffer (GSOF) <gsoffer@yahoo.com>
2024-04-17 21:21:04 -0700

    Revert back to object storing points and connection (lines) separately. This reduces the number of points to transform.

--- Lib3D/Object_base.py
+++ Lib3D/Object_base.py
@@ -1,41 +1,5 @@
 from MathLib import MathLib as ML
 
-def scale(points, scale) -> list:
-    newPoints = [None]*len(points)
-    for i,point in enumerate(points):
-        #newPoints[i] = ML.scale_V3(point, scale)
-        p0 = ML.scale_V3(point[0], scale)
-        p1 = ML.scale_V3(point[1], scale)
-        newPoints[i] = (p0, p1)
-    return newPoints
-
-def rotate(points, x=0, y=0, z=0, dcm=None, state=False) -> list:
-    newPoints = [None]*len(points)
-    if dcm == None:
-        dcm = ML.DCM_XYZ(x, y, z)
-
-    for i, point in enumerate(points):
-        #newPoints[i] = ML.MxV(dcm, point)
-        p0 = ML.MxV(dcm, point[0])
-        p1 = ML.MxV(dcm, point[1])
-        newPoints[i] = (p0, p1)
-    return newPoints
-
-def translate(points, x=0, y=0, z=0, V=None, state=False) -> list:
-    newPoints = [None]*len(points)
-    if V == None:
-        V = (x, y, z)
-
-    for i, point in enumerate(points):
-        #newPoints[i] = ML.addV(point, V)
-        p0 = ML.addV(point[0], V)
-        p1 = ML.addV(point[1], V)
-        newPoints[i] = (p0, p1)
-    return newPoints
-
-def getLines(self) -> list:
-    return []
-
 class Object_base():
     def __init__(self, obj=None, filename=None):
         return
@@ -78,7 +42,10 @@ class Object_container(Object_base):
         return self
 
     def getShape(self):
-        return []
+        shapes = []
+        for shape in self.shapes:
+            shapes += shape.getShape()
+        return shapes
 
     def getLines(self):
         lines = []
