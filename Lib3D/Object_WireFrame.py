75344dcc0f923c8f1a075cf6726394e7d497ad80
Tzur <103438808+RandomTheFirst@users.noreply.github.com>
2024-04-27 13:58:48 -0700

    added stl loading nad net to json

--- Lib3D/Object_WireFrame.py
+++ Lib3D/Object_WireFrame.py
@@ -2,11 +2,19 @@ from MathLib import MathLib as ML
 from Lib3D import Object_base as O
 from Lib3D import Lib3D as L
 import json
+try:
+    from stl import mesh
+except:
+    print("could not find numpy-stl. DO NOT USE STL FILES!!\nTo install, run ```pip install numpy-stl```")
 
 class Object_wireFrame(O.Object_base):
     def __init__(self, obj=None, filename=None, color=(0,0,0)):
         if filename != None:
-            obj = self._loadJson(filename)
+            ext = filename.split(".")[-1]
+            if ext == "json":
+                obj = self._loadJson(filename)
+            elif ext == "stl":
+                obj = self._loadStl(filename)
 
         self.color  = color
         self.initShape   = obj["points_xyz"]
@@ -22,6 +30,31 @@ class Object_wireFrame(O.Object_base):
         with open(filename) as f:
             obj = json.load(f)
         return obj
+    
+    def _loadStl(self, filename):
+        objMesh = mesh.Mesh.from_file(filename) #< Load the STL file
+
+        # Initialize lists for points and lines
+        points = []
+        lines = []
+
+        for i in range(len(objMesh.vectors)):
+            vertexes = objMesh.vectors[i].tolist() #< triangle
+            # Add the vertices (points) to the list
+            for vertex in vertexes:
+                points.append(vertex)
+            
+            # Add the edges (lines) to the list
+            lines.append((i*3, i*3+1))
+            lines.append((i*3+1, i*3+2))
+            lines.append((i*3+2, i*3+0))
+
+        # Now you have a list of points and lines
+        obj = {"scale": 100.0,
+                "points_xyz": points,
+                "connections": lines}
+
+        return(obj)
 
     def reset(self):
         self.shape = self.initShape
