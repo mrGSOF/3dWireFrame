a499bf970026f595ffb161b31d99c270c3b4c32c
Guy Soffer (GSOF) <gsoffer@yahoo.com>
2024-06-18 20:49:21 -0700

    Refactor example to use player's controls of movement and attitude.

--- Example_motionWorld.py
+++ Example_motionWorld.py
@@ -1,14 +1,14 @@
-import pygame, math
+import pygame
 from Lib3D import Object_WireFrame as OWF
 from Lib3D import Object_base as OB
 from Lib3D import Objects
 from Lib3D import WireFrame_display as DISP
+from modules import PlayerControl
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 WHITE = (255,255,255)
 BLACK = (0,0,0)
-PI = math.pi
 
 def clearScreen(screen, color=(255,255,255)) -> None:
   screen.fill(color)
@@ -30,56 +30,22 @@ if __name__ == "__main__":
     clock = pygame.time.Clock()
     screen = newScreen("3D Wire Frame Shapes", SCREEN_WIDTH, SCREEN_HEIGHT, WHITE)
     wireframe = DISP.WireFrame(screen, pygame.draw.line, f=50, scale=10)
+    player = PlayerControl.PlayerControl( center=(int(screen.get_width()/2), int(screen.get_height()/2)) )
 
     fps = 30
     dt = 1/fps
     t = 0.0
-    camAngX_r = 0.0
-    camAngY_r = 0.0
-    roofAng_r = 0.0
-    houseAng_r = 0.0
-    centerX = int(SCREEN_WIDTH/2)
-    centerY = int(SCREEN_HEIGHT/2)
     run = True
-    location = [0,200,0]
-    attitude = [0.0, 0.0, 0.0]
 
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
-##            elif event.type == pygame.KEYDOWN:
-##                speed = 1
-##                if event.key == pygame.K_LEFT:
-##                    location[0] += speed
-##                if event.key == pygame.K_RIGHT:
-##                    location[0] -= speed
-##                if event.key == pygame.K_UP:
-##                    location[2] += speed
-##                if event.key == pygame.K_DOWN:
-##                    location[2] -= speed
-##
-##                speed = 0.05
-##                if event.key == pygame.K_q:
-##                    attitude[0] += speed
-##                if event.key == pygame.K_z:
-##                    attitude[0] -= speed
-
         keys = pygame.key.get_pressed()
-        speed = 1
-        location[0] += -(keys[pygame.K_RIGHT] - keys[pygame.K_LEFT])*2.0*speed
-        location[1] += -(keys[pygame.K_UP] - keys[pygame.K_DOWN])*2.0*speed
-        location[2] += -(keys[pygame.K_q] - keys[pygame.K_z])*5*speed
         wireframe.f += -(keys[pygame.K_w] - keys[pygame.K_s])*10
-        #attitude[0] += -(keys[pygame.K_q] - keys[pygame.K_z])*0.05*speed
-        #print(wireframe.f, location[0], location[1], location[2])
-        mousePos = pygame.mouse.get_pos()
-        mousePos = (0.5*PI*(mousePos[0]/centerX -1), 0.5*PI*(1 -mousePos[1]/centerY))
-        #print(mousePos)
-        #attitude[1] = mousePos[0]
-        #attitude[0] = mousePos[1]
-
+        location, attitude = player.update()
+        
         clearScreen(screen, WHITE)
         world.reset()
         world.translate(V=location, initShape=False)
--- modules/PlayerControl.py
+++ modules/PlayerControl.py
@@ -1,16 +1,15 @@
 import pygame, math
-from MathLib import MathLib as M
-PI = math.pi
+from MathLib import MathLib as ML
 
 class PlayerControl():
-  def __init__(pos=(0,0,0), att=(0,0,0), center=(0,0)):
+  def __init__(self, pos=(0,0,0), att=(0,0,0), center=(0,0)):
     self.posX = pos[0]
     self.posY = pos[1]
     self.posZ = pos[2]
     self.attX = att[0]
     self.attY = att[1]
-    self.centerX = centerX
-    self.centerY = centerY
+    self.centerX = center[0]
+    self.centerY = center[1]
 
   def update(self) -> list:
     pos = self.updatePos()
@@ -19,8 +18,8 @@ class PlayerControl():
 
   def updateAtt(self) -> list:
     (mPosX, mPosY) = pygame.mouse.get_pos()
-    self.attY = PI*(mPosX/self.centerX -1)
-    self.attX = PI*(mPosY/self.centerY -1)
+    self.attY = 0.5*ML.pi*(mPosX/self.centerX -1)
+    self.attX = 0.5*ML.pi*(mPosY/self.centerY -1)
     return (self.attX, self.attY)
 
   def updatePos(self) -> list:
