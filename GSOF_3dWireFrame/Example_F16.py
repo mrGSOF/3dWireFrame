32c9ba4b099f08df7bcb255364b1e5bbfc77768f
Guy Soffer <gsoffer@yahoo.com>
2026-05-31 19:33:21 -0700

    Add example of an F16 3D object that can be manipulated.

---
+++ GSOF_3dWireFrame/Example_F16.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python
+"""
+ * Example_F16.py
+ * Created on: 31 May 2026
+ * Improved for: 31 May 2026
+ * Author: Guy Soffer
+ * Copyright (C) 2026 Guy Soffer
+"""
+import pygame, math
+from Lib3D import Object_WireFrame as OWF
+from Lib3D import Object_base as OB
+from Lib3D import Objects
+from Lib3D import WireFrame_display as DISP
+from F16_Class import F16_View
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+PI = math.pi
+
+class Engine():
+    def __init__(self, thrust_lbf):
+        self.thrust_lbf = thrust_lbf
+
+class InertialNavigationSystem():
+    def __init__(self, azimuth_d, pitch_d, roll_d):
+        self.azimuth_d = azimuth_d
+        self.pitch_d   = pitch_d
+        self.roll_d    = roll_d
+
+class FlightSurfaces():
+    def __init__(self,
+                 leftAliron_d=0, rightAliron_d=0,
+                 leftElevator_d=0, rightElevator_d=0,
+                 rudder_d=0, speedbrake_d=0,
+                 gearDown_b=True):
+        self.lAliron_d   = leftAliron_d
+        self.rAliron_d   = rightAliron_d
+        self.lElevator_d = leftElevator_d
+        self.rElevator_d = rightElevator_d
+        self.rudder_d    = rudder_d
+        self.sb_d        = speedbrake_d
+        self.gearsDown   = gearDown_b
+
+class WeightOnWheels():
+    def __init__(self, left, right, nose):
+        self.left  = left
+        self.right = right
+        self.nose  = nose
+
+def drawWireFrame(screen, obj, color=None) -> None:
+    for line in obj.getLines():
+        x0, y0, z0 = line.p0
+        x1, y1, z1 = line.p1
+        lcolor = color
+        if lcolor == None:
+            lcolor = line.color
+        pygame.draw.line( screen, lcolor, (x0, y0), (x1, y1) ) #< Line from P0 to P1
+
+def clearScreen(screen, color=(255,255,255)) -> None:
+  screen.fill(color)
+    
+def newScreen(title="New", resX=SCREEN_WIDTH, resY=SCREEN_HEIGHT, color=WHITE):
+    screenSize = (resX, resY)
+    screen = pygame.display.set_mode( screenSize )
+    clearScreen(screen, color)
+    pygame.display.set_caption(title)
+    return screen
+
+if __name__ == "__main__":
+    ground = OWF.Object_wireFrame(
+       obj=Objects.net(25,25), color=(0,100,0))\
+       .rotate(x=math.pi/2, y=0, z=0)\
+       .translate(V=(-1000, -200, -1000), initShape=True)\
+       .scale(0.2, initShape=True)
+
+    f16 = F16_View()
+
+    world = OB.Object_container(objList=(ground, f16))
+
+    pygame.init()
+    clock = pygame.time.Clock()
+    screen = newScreen("3D Wire Frame Shapes", SCREEN_WIDTH, SCREEN_HEIGHT, WHITE)
+    wireframe = DISP.WireFrame(screen, pygame.draw.line, f=50, scale=10.0)
+
+    fps = 30
+    dt = 1/fps
+    t = 0.0
+    camAngX_r = 0.0
+    camAngY_r = 0.0
+    roofAng_r = 0.0
+    houseAng_r = 0.0
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+              run = False
+        world.reset()
+        eng  = Engine(thrust_lbf=6000)
+        wow = WeightOnWheels(left=True, right=True, nose=True)
+        f16.update(t, fcs=None, eng=eng, ins=None, wow=wow)
+##        #world.rotate(x=1.8,y=3.14,z=0.3)
+        world.rotate(x=camAngX_r,y=camAngY_r,z=0)
+        world.translate(x=0,y=0,z=-600)
+
+        ### Draw 3D world
+        clearScreen(screen, WHITE)
+        wireframe.draw(world)
+        
+        ### Wait for next step time
+        camAngX_r += 0.5*dt
+        camAngY_r += 1*dt
+        t += dt
+        clock.tick(30)
+        
+        ### Display output
+        pygame.display.flip()
+
+    pygame.quit()
+    
---
+++ GSOF_3dWireFrame/F16_Class.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python
+"""
+ * F16_Class.py
+ * Created on: 6 Jan 2025
+ * Improved for: 25 May 2026
+ * Author: Guy Soffer
+ * Copyright (C) 2026 Guy Soffer
+"""
+
+from math import pi
+try:
+   from GSOF_3dWireFrame.Lib3D import Object_WireFrame as OWF
+   from GSOF_3dWireFrame.Lib3D import Object_base as OB
+   from GSOF_3dWireFrame.Lib3D import Objects
+   _3D_active = True
+except:
+   _3D_active = False
+   print("GSOF_Wireframe3D module isn't installed")
+
+degToRad = pi/180
+class F16_View(OB.Object_container):
+    """Constructs the gauges screen"""
+    def __init__(self, folder='./'):
+        self.time = 0.0
+        axis  = OWF.Object_wireFrame(
+           filename="%s/objects/axis.json"%folder, color=(10,10,10 ))\
+           .translate(V=(0, 0, 0))\
+           .scale(1.5, initShape=True)
+
+        self.plane = OWF.Object_wireFrame(
+            filename="%s/objects/f16.stl"%folder, color=( 0, 0,255))\
+            .scale(1.5)\
+            .rotate(x=0, y=0, z=0)\
+            .translate(V=(0, 0, 0), initShape=True)
+        self.plane.setOrigin(
+            origin=self.plane.getOrigin(origin="arithCenter") )\
+            .scale(0.01, initShape=True)
+        
+        plume = OWF.Object_wireFrame(
+           filename="%s/objects/Plume.json"%folder, color=( 255, 0,0))\
+           .scale(0.5)\
+           .rotate(x=0, y=180*degToRad, z=0)\
+           .translate(V=(0, 0, -130), initShape=True)
+                       
+        self.plume = OB.Object_container(objList=(plume,))
+        
+        nw = OWF.Object_wireFrame(
+           filename="%s/objects/LandingGear.json"%folder, color=( 0,0,0))\
+           .scale(0.1)\
+           .translate(V=(0, -45, 120), initShape=True)
+        self.nwow = OWF.Object_wireFrame(
+           filename="%s/objects/Spark.json"%folder, color=( 255,0,255))\
+           .scale(0.1, initShape=True)#\
+           #.translate(V=(0, -48, 120), initShape=True) #< Translation is done in manipulation function
+        rw = OWF.Object_wireFrame(
+           filename="%s/objects/LandingGear.json"%folder, color=( 0,0,0))\
+           .scale(0.1)\
+           .rotate(x=0, y=0, z=-degToRad*10)\
+           .translate(V=(-15, -45, 15), initShape=True)
+        self.rwow = OWF.Object_wireFrame(
+           filename="%s/objects/Spark.json"%folder, color=( 255,255,0))\
+           .scale(0.1, initShape=True)#\
+           #.translate(V=(-15, -48, 15), initShape=True) #< Translation is done in manipulation function
+        lw = OWF.Object_wireFrame(
+           filename="%s/objects/LandingGear.json"%folder, color=( 0,0,0))\
+           .scale(0.1)\
+           .rotate(x=0, y=0, z=degToRad*10)\
+           .translate(V=(15, -45, 15), initShape=True)
+        self.lwow = OWF.Object_wireFrame(
+           filename="%s/objects/Spark.json"%folder, color=( 255,255,0))\
+           .scale(0.1, initShape=True)#\
+           #.translate(V=(15, -48, 15), initShape=True) #< Translation is done in manipulation function
+
+        self.gears = OB.Object_container(objList=(nw, self.nwow,
+                                                  rw, self.rwow,
+                                                  lw, self.lwow
+                                                  ))
+        
+        super().__init__(objList=(self.plane, self.plume, self.gears))
+
+    def update(self, time, fcs=None, eng=None, ins=None, wow=None):
+        """Update all elements"""
+        self.time += 0.1
+        if eng != None:
+            self.setExahustPlume(eng.thrust_lbf)
+
+        if wow != None:
+           leftWow, rightWow, noseWow = wow.left, wow.right, wow.nose
+           self.setWOW(leftWow, rightWow, noseWow)
+
+        if fcs != None:
+            lElev_deg  = fcs.lElevator_d
+            rElev_deg  = fcs.rElevator_d
+            rudder_deg = fcs.rudder_d
+            sb_deg     = fcs.speedbrake_d
+            self.setFCS(lElev_deg, rElev_deg, rudder_deg, sb_deg)
+            self.setGearsDown(fcs.gearDown_b)
+
+        if ins != None:
+            heading =  ins.heading_d*degToRad
+            pitch   = -ins.pitch_d*degToRad
+            roll    =  ins.roll_d*degToRad
+            self.setAttitude(heading, pitch, roll)
+
+            mToFt = 3.28
+            self.setAltitude(mToFt*ins.height.value)
+
+    def setAltitude(self, alt_ft) -> None:
+        self.translate(V=(0, alt_ft, 0))
+       
+    def setAttitude(self, heading, pitch, roll) -> None:
+        self.rotate( y=-heading-0*degToRad, x=-pitch, z=-roll )
+
+    def setExahustPlume(self, thrust_lbf) -> None:
+        thrustMAX = 6360
+        plume = thrust_lbf/thrustMAX
+        plumeColor = (255, 255*(1-plume), 255*(1-plume))
+        self.plume.rotate(x=0, y=0, z=self.time*plume)
+        self.plume.scale(plume).color = plumeColor
+       
+    def setFCS(self, lElev_deg, rElev_deg, rudder_deg, sb_deg) -> None:
+        return
+      
+    def setGearsDown(self, downCmd) -> None:
+        if downCmd == False:
+           self.gears.scale(0.0)
+        else:
+           self.gears.scale(1.0)
+
+    def setWOW(self, leftWow, rightWow, noseWow) -> None:
+        time = int(self.time*10)
+        blink = bool(time&0b010)
+        self.nwow.rotate(x=0, y=0, z=4*self.time).translate(V=(0, -48, 120)).scale(int(noseWow and blink))
+        self.lwow.rotate(x=0, y=0, z=4*self.time).translate(V=(-15, -48, 15)).scale(int(leftWow and blink))
+        self.rwow.rotate(x=0, y=0, z=4*self.time).translate(V=(15, -48, 15)).scale(int(rightWow and blink))
+
