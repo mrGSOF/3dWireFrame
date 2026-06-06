66f005a0beb146a4e081ab6a28eb08f53879f1a0
guy.soffer <Guy.Soffer@stratolaunch.com>
2026-06-05 17:23:27 -0700

    code

--- GSOF_3dWireFrame/Lib3D/Object_base.py
+++ GSOF_3dWireFrame/Lib3D/Object_base.py
@@ -12,16 +12,34 @@ class Object_base():
             self.stateOrigin = copy.copydeep(self.state)
         return self
 
-    def getOrigin(self ):
+    def getOrigin(self) -> list:
         """Get the original state"""
         return self.stateOrigin
 
-    def reset(self, newState=None):
-        """Reset current state to original or new state"""
-        state = self.stateOrigin if newState == None else newState
-        self.state = copy.copydeep(state) #< 4x4 matrix to store the currect state
+    def reset(self, all=True):
+        """Reset current state to original"""
+        self.state = copy.copydeep(stateOrigin) #< 4x4 matrix to store the currect state
         return self
-    
+
+    def _findCenter(self,
+                    method: str="arithCenter",
+                    points: list|tuple
+                    ) -> list:
+        """Return the center point of all points"""
+        if origin == "arithCenter":
+            return L.findArithmeticCenter(points)
+            
+        elif origin == "minMaxCenter":
+            return L.findMinMaxCenter(points)
+
+        elif origin == None:
+            return self.getOrigin()
+
+    def copyDcmIntoState(self, dcm) -> None:
+        for ri, row in enumerate(dcm):
+            for ci, val in enumerate(row):
+                self.state[ri][ci] = val
+
     def scale(self, scale, setInitState=False):
         """Apply scaling to curent state"""
         self.state[0][0] *= scale[0]
@@ -29,9 +47,12 @@ class Object_base():
         self.state[2][2] *= scale[2]
         self.setOrigin(setInitState)
         return self
-
+    
     def rotate(self, x, y, z, dcm, setInitState=False):
         """Apply rotation to curent state"""
+        if dcm == None:
+            dcm = ML.DCM_YXZ(y, x, z) #< This is the proper rotation order for our coordinate system
+        self.copyDcmIntoState(ML.MxM(self.state, dcm)
         self.setOrigin(setInitState)
         return self
 
@@ -43,7 +64,23 @@ class Object_base():
         self.setOrigin(setInitState)
         return self
 
-    def transform(self, scle, rotate, translate, setInitState=False):
+    def transform(self, scale, rotate, translate, setInitState=False):
         """Apply transformation to curent state"""
+        self.state\
+        .scale(scale, si)\
+        .rotate(rotate, si)\
+        .translate(translate, si)
         self.setOrigin(setInitState)
         return self
+
+    def getLines(self) -> list:
+        """Return all lines of object"""
+        return []
+
+    def update(self, state) -> None:
+        """Update the coordinates of all points of the object"""
+        self.state = L.updateTransformationMatrix(self.state, state)
+
+    def getObjects(self) -> list: #< For compatibility with Assembly class
+        """Return a list of all objects"""
+        return []
