831b1c4690eea427db78e021f53ac629fbbd3519
Guy Soffer <gsoffer@yahoo.com>
2026-06-07 21:40:58 -0700

    Updated Example_F16.py

--- GSOF_3dWireFrame/Example_F16.py
+++ GSOF_3dWireFrame/Example_F16.py
@@ -7,8 +7,9 @@
  * Copyright (C) 2026 Guy Soffer
 """
 import pygame, math
-from Lib3D import Object_WireFrame as OWF
-from Lib3D import Object_base as OB
+from Lib3D.Object_WireFrame import Object_wireFrame
+from Lib3D.Assembly import Assembly
+#from Lib3D.Object_base import Object_base
 from Lib3D import Objects
 from Lib3D import WireFrame_display as DISP
 from F16_Class import F16_View
@@ -16,6 +17,7 @@ from F16_Class import F16_View
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 WHITE = (255,255,255)
+BLUE = (220,220,255)
 BLACK = (0,0,0)
 PI = math.pi
 
@@ -69,48 +71,60 @@ def newScreen(title="New", resX=SCREEN_WIDTH, resY=SCREEN_HEIGHT, color=WHITE):
     return screen
 
 if __name__ == "__main__":
-    ground = OWF.Object_wireFrame(
+    ground = Object_wireFrame(
        obj=Objects.net(25,25), color=(0,100,0))\
+       .scale(0.2)\
        .rotate(x=math.pi/2, y=0, z=0)\
-       .translate(V=(-1000, -200, -1000), initShape=True)\
-       .scale(0.2, initShape=True)
+       .translate(-250, -200, -250)\
+       .setOrigin()
 
-    f16 = F16_View()
+    f16 = F16_View()\
+          .scale(1.0)\
+          .translate(0, 0, 0)\
+          .rotate(0*3.14/2, 0*3.14/2, 0)\
+          .setOrigin()
 
-    world = OB.Object_container(objList=(ground, f16))
+    world = Assembly(objects=(ground, f16))
 
     pygame.init()
     clock = pygame.time.Clock()
     screen = newScreen("3D Wire Frame Shapes", SCREEN_WIDTH, SCREEN_HEIGHT, WHITE)
-    wireframe = DISP.WireFrame(screen, pygame.draw.line, f=50, scale=10.0)
+    wireframe = DISP.WireFrame(screen, pygame.draw.line, f=50, scale=20.0)
 
     fps = 30
     dt = 1/fps
     t = 0.0
     camAngX_r = 0.0
     camAngY_r = 0.0
-    roofAng_r = 0.0
-    houseAng_r = 0.0
-    run = True
+    eng  = Engine(thrust_lbf=6000)
+    wow = WeightOnWheels(left=True, right=True, nose=True)
+    useMouse = True
 
+    run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
               run = False
         world.reset()
-        eng  = Engine(thrust_lbf=6000)
-        wow = WeightOnWheels(left=True, right=True, nose=True)
-        f16.update(t, fcs=None, eng=eng, ins=None, wow=wow)
-##        #world.rotate(x=1.8,y=3.14,z=0.3)
-        world.transform(rotate=(camAngX_r,camAngY_r,0), translate=(0,0,-600))
+        f16.setControls(t, fcs=None, eng=eng, ins=None, wow=wow)
+        world.transform(rotate=(camAngX_r,camAngY_r,0), translate=(0,0,-1000))
+#        world.transform(rotate=(0.0*3.14, 0.0*3.14, 0), translate=(0,0,-80))
 
         ### Draw 3D world
-        clearScreen(screen, WHITE)
+        clearScreen(screen, BLUE)
         wireframe.draw(world)
         
         ### Wait for next step time
-        camAngX_r += 0.5*dt
-        camAngY_r += 1*dt
+        keys = pygame.key.get_pressed()
+        eng.thrust_lbf += -(keys[pygame.K_1] - keys[pygame.K_2])*200
+        
+        if useMouse:
+            (mPosX, mPosY) = pygame.mouse.get_pos()
+            camAngY_r = 0.01*(mPosX -SCREEN_WIDTH/2)
+            camAngX_r = 0.01*(mPosY -SCREEN_HEIGHT/2)
+        else:
+            camAngX_r += 0.5*dt
+            camAngY_r += 1*dt
         t += dt
         clock.tick(30)
         
