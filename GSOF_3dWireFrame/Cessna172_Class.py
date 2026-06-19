db219e2d25f29781bd6daba0ca74b1a62c5ccac9
Tzur Soffer <tzur.soffer@gmail.com>
2026-06-18 18:50:58 -0700

    Dial in positions

--- GSOF_3dWireFrame/Cessna172_Class.py
+++ GSOF_3dWireFrame/Cessna172_Class.py
@@ -9,11 +9,11 @@
 
 from math import pi
 try:
-   from GSOF_3dWireFrame.Lib3D.Object_WireFrame import Object_wireFrame as Object
-   from GSOF_3dWireFrame.Lib3D.Assembly import Assembly
-   from GSOF_3dWireFrame.utils import Colors
+    from GSOF_3dWireFrame.Lib3D.Object_WireFrame import Object_wireFrame as Object
+    from GSOF_3dWireFrame.Lib3D.Assembly import Assembly
+    from GSOF_3dWireFrame.utils import Colors
 except:
-   raise "GSOF_Wireframe3D module isn't installed"
+    raise "GSOF_Wireframe3D module isn't installed"
 
 class View(Assembly):
     """Constructs the gauges screen"""
@@ -27,23 +27,79 @@ class View(Assembly):
            .setOrigin()
 
         self.plane = Object(
-            filename="%s/objects/Cessna 172/body.stl"%folder, color=Colors.BLUE, name="Cessna172")\
+            filename="%s/objects/Cessna 172/body.stl"%folder, color=Colors.BLUE)\
             .setCenter(scale=1.0, method="arithCenter")
         
         self.propeller = Object(
-           filename="%s/objects/Cessna 172/propeller.stl"%folder, color=Colors.RED, name="Propeller")\
+           filename="%s/objects/Cessna 172/propeller.stl"%folder, color=Colors.RED)\
             .setCenter(scale=1.0, method="arithCenter")\
-            .translate(0, -20, 225)\
+            .translate(0, -20, 210)\
             .setOrigin()
         self.propeller.color = Colors.RED
 
-        self.gears = Object(
-           filename="%s/objects/Cessna 172/wheels.stl"%folder, color=Colors.BLACK, name="GEARS")\
+        self.leftGear = Object(
+           filename="%s/objects/Cessna 172/LeftWheel.stl"%folder, color=Colors.BLACK)\
+              .setCenter(scale=1.0, method="arithCenter")\
+              .translate(70, -80, 0)\
+              .setOrigin()
+        self.rightGear = Object(
+           filename="%s/objects/Cessna 172/RightWheel.stl"%folder, color=Colors.BLACK)\
+              .setCenter(scale=1.0, method="arithCenter")\
+              .translate(-70, -80, 0)\
+              .setOrigin()
+        self.frontGear = Object(
+           filename="%s/objects/Cessna 172/FrontWheel.stl"%folder, color=Colors.BLACK)\
+              .setCenter(scale=1.0, method="arithCenter")\
+              .translate(0, -80, 120)\
+              .setOrigin()
+        self.gears = Assembly((self.leftGear,
+                           self.rightGear,
+                           self.frontGear
+                          ))
+
+        self.rightFlap = Object(
+           filename="%s/objects/Cessna 172/RightFlap.stl"%folder, color=Colors.RED)\
             .setCenter(scale=1.0, method="arithCenter")\
-            .translate(0, -80, 80)\
+            .translate(-95, 25, 0)\
+            .setOrigin()
+        self.leftFlap = Object(
+           filename="%s/objects/Cessna 172/LeftFlap.stl"%folder, color=Colors.RED)\
+            .setCenter(scale=1.0, method="arithCenter")\
+            .translate(95, 25, 0)\
+            .setOrigin()
+        flaps = Assembly((self.rightFlap, self.leftFlap))
+
+        self.rightAileron = Object(
+           filename="%s/objects/Cessna 172/RightAileron.stl"%folder, color=Colors.RED)\
+            .setCenter(scale=1.0, method="arithCenter")\
+            .translate(-275, 25, 10)\
+            .setOrigin()
+        self.leftAileron = Object(
+           filename="%s/objects/Cessna 172/LeftAileron.stl"%folder, color=Colors.RED)\
+            .setCenter(scale=1.0, method="arithCenter")\
+            .translate(275, 25, 10)\
+            .setOrigin()
+        ailerons = Assembly((self.rightAileron, self.leftAileron))
+
+        self.rightElevator = Object(
+           filename="%s/objects/Cessna 172/RightElevator.stl"%folder, color=Colors.RED)\
+            .setCenter(scale=1.0, method="arithCenter")\
+            .translate(-60, -20, -270)\
+            .setOrigin()
+        self.leftElevator = Object(
+           filename="%s/objects/Cessna 172/LeftElevator.stl"%folder, color=Colors.RED)\
+            .setCenter(scale=1.0, method="arithCenter")\
+            .translate(60, -20, -270)\
+            .setOrigin()
+        elevators = Assembly((self.rightElevator, self.leftElevator))
+
+        self.rudder = Object(
+           filename="%s/objects/Cessna 172/Rudder.stl"%folder, color=Colors.RED)\
+            .setCenter(scale=1.0, method="arithCenter")\
+            .translate(0, 25, -290)\
             .setOrigin()
 
-        super().__init__(objects=(axis, self.plane, self.propeller, self.gears))
+        super().__init__(objects=(axis, self.plane, self.propeller, self.gears, flaps, ailerons, elevators, self.rudder))
 
     def tick(self, fps=60):
         """Update all elements"""
